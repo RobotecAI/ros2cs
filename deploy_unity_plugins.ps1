0f4cb9338d71d9b68b1cde725df1193fd9a30aa8
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2021-06-14 12:39:42 +0200

    Deploy plugin for windows

---
+++ deploy_unity_plugins.ps1
@@ -0,0 +1,30 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$pluginDir=$args[0]
+
+function Print-Help {
+"
+Usage: 
+deploy_unity_plugins.ps1 [PLUGINS_DIR]
+
+PLUGINS_DIR - Assets/Plugins/ directory of Unity project.
+"
+
+}
+
+if (([string]::IsNullOrEmpty($pluginDir)))
+{
+    Print-Help
+    exit
+}
+
+if (Test-Path -Path $pluginDir) {
+    (Copy-Item -verbose -Path $scriptPath\install\lib\dotnet\* -Destination ${pluginDir} 4>&1).Message
+    Write-Host "Plugins copied to: '$pluginDir'" -ForegroundColor Green
+    if(-not (Test-Path -Path $pluginDir\Windows\x86_64\)) {
+        mkdir ${pluginDir}\Windows\x86_64\
+    }
+    (Copy-Item -verbose -Path $scriptPath\install\lib\*.lib -Destination ${pluginDir}\Windows\x86_64\ 4>&1).Message
+    Write-Host "Libraries copied to '${pluginDir}\Windows\x86_64\'" -ForegroundColor Green
+} else {
+    Write-Host "Plugins directory: '$pluginDir' doesn't exist." -ForegroundColor Red
+}
