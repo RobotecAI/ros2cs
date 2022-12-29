5ce3a792d081726c68fda9943410f08d3ef8cc67
Eric Wolf <ewolf@advitec.de>
2022-12-29 17:11:07 +0100

    add IWaitable
    This interface contains all methods necessary
    for executors to process work.
    The async version is equivalent to the non-async version
    and an optimization opportunity for task based executors.
    
     create mode 100644 src/ros2cs/ros2cs_core/interfaces/IWaitable.cs

---
+++ src/ros2cs/ros2cs_core/interfaces/IWaitable.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+
+namespace ROS2
+{
+    /// <summary>
+    /// Object which can process becoming ready.
+    /// </summary>
+    public interface IWaitable
+    {
+        /// <summary>
+        /// Try to process if this instance is ready.
+        /// </summary>
+        /// <remarks>Both variants of this method are equivalent.</remarks>
+        /// <returns>If the instance was ready.</returns>
+        bool TryProcess();
+
+        /// <summary>
+        /// Try to process preferably asynchronously if this instance is ready.
+        /// </summary>
+        /// <returns><see cref="Task"/> indicating if the instance was ready.</returns>
+        /// <inheritdoc cref="TryProcess"/>
+        Task<bool> TryProcessAsync();
+    }
+}
