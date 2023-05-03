b214701bd4357abb4e2d0e2c09f6523703d05881
Eric Wolf <ewolf@advitec.de>
2023-05-03 15:31:09 +0200

    reduce amount of packages being build
    This commit only builds packages contained in
    `src/ros2cs` and `src/custom_packages` and their
    dependencies to speed up the build process.
    Furthermore, it makes building the examples optional.

--- build.ps1
+++ build.ps1
@@ -8,16 +8,21 @@
     build with tests
 .PARAMETER standalone
     standalone build
+.PARAMETER with_examples
+    build with examples
 #>
 Param (
     [Parameter(Mandatory=$false)][switch]$with_tests=$false,
-    [Parameter(Mandatory=$false)][switch]$standalone=$false
+    [Parameter(Mandatory=$false)][switch]$standalone=$false,
+    [Parameter(Mandatory=$false)][switch]$with_examples=$false
 )
 
 $msg="Build started."
+$packages=,"ros2cs_core"
 $tests_switch=0
 if($with_tests) {
     $msg+=" (with tests)"
+    $packages+="ros2cs_tests"
     $tests_switch=1
 }
 $standalone_switch=0
@@ -25,6 +30,20 @@ if($standalone) {
     $msg+=" (standalone)"
     $standalone_switch=1
 }
+if ($with_examples) {
+    $msg+=" (with examples)"
+    $packages+="ros2cs_examples"
+}
 
 Write-Host $msg -ForegroundColor Green
-colcon build --merge-install --event-handlers console_direct+ --cmake-args -DSTANDALONE_BUILD:int=$standalone_switch -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING:int=$tests_switch --no-warn-unused-cli
+&"colcon" build `
+--merge-install `
+--event-handlers console_direct+ `
+--cmake-args `
+-DSTANDALONE_BUILD:int=$standalone_switch `
+-DCMAKE_BUILD_TYPE=Release `
+-DBUILD_TESTING:int=$tests_switch `
+--no-warn-unused-cli `
+--packages-up-to `
+@packages `
+@(colcon list --names-only --base-paths src/custom_packages)
